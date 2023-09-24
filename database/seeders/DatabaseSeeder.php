fd6d2b266912334435f1c62ce8b6a4f8fb8446bb
Hector Nevado <hectornevado46@gmail.com>
2023-09-24 17:23:04 +0200

    Modificación modelos para las relaciones y creación del seeder para los datos de prueba

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,10 @@ namespace Database\Seeders;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
+use App\Models\Tag;
+use App\Models\Thread;
+use App\Models\Category;
+
 class DatabaseSeeder extends Seeder
 {
     /**
@@ -12,11 +16,27 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+         //\App\Models\User::factory(10)->create();
+         
+         Tag::factory(6)->create();
+         Category::factory(5)->has(
+            Thread::factory(10)->hasComments(8)
+         )->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+         
+        $tags = Tag::all();
+
+        Thread::all()->each(function ($thread) use ($tags) {
+
+            $tag = $tags->random(
+                rand(1,6)
+            )->pluck('id')->toArray(); //id para la relación
+
+            $thread->tags()->attach($tag);
+
+        });
+        
+
+
     }
 }
